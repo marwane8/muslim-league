32a12cb3308e727d55026c81f17aa07ea104864c
Marwane <taroua.marwane@gmail.com>
2022-03-02 18:44:43 -0500

    core-api cors config

--- muslim-league-core-api/src/main/java/com/marwane/ml_core_api/WebConfig.java
+++ muslim-league-core-api/src/main/java/com/marwane/ml_core_api/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
     }
--- muslim-league-core-api/src/main/java/com/marwane/ml_core_api/controller/GamesController.java
+++ muslim-league-core-api/src/main/java/com/marwane/ml_core_api/controller/GamesController.java
@@ -1,11 +1,15 @@
 package com.marwane.ml_core_api.controller;
 
+import com.marwane.ml_core_api.MuslimLeagueApplication;
 import com.marwane.ml_core_api.model.view.playerTotals;
 import com.marwane.ml_core_api.model.view.schedule;
 import com.marwane.ml_core_api.service.games.GamesService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -15,8 +19,13 @@ public class GamesController {
     @Autowired
     private GamesService gamesService;
 
+    private static final Logger log = LoggerFactory.getLogger(MuslimLeagueApplication.class);
+
     @GetMapping
-    public List<schedule> Games() { return gamesService.getGames();}
+    public List<schedule> Games(HttpServletRequest request) {
+        String address = request.getRemoteAddr();
+        log.info("Incoming request to games from: " + address);
+        return gamesService.getGames();}
 
     @GetMapping("{gameID}/stats")
     public List<playerTotals> getBoxScore(@PathVariable("gameID") int GameID ) { return gamesService.getBoxScore(GameID); }
