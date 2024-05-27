5ca5b13275cfc30ebb8b3d24b0e8e15f62b0df63
Marwane <taroua.marwane@gmail.com>
2024-05-27 16:33:57 -0400

    fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/muslim-league/",
   plugins: [
     remix({
+      basename: "/muslim-league/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
